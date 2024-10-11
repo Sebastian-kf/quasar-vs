2d8f5fe6907f0e413b9f94b58ea8c4bc68314e01
Sebastian Fortuin <sebastian@rlabs.org>
2024-10-11 11:06:13 +0200

    qbtn

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,7 +6,17 @@
   <q-page class="flex flex-center">
     <q-btn
       label="Hello Quasar!"
-      color="red"
-    />
+      color="blue-grey-6"
+      size="xl"
+      rounded
+      icon="book"
+    >
+      <q-badge
+        floating
+        color="red"
+      >
+        88
+      </q-badge>
+    </q-btn>
   </q-page>
 </template>
